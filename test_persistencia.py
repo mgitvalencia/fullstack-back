fba7dee05623ea030012cabaac30a57c2b79cd6e
mgitvalencia <105307230+mgitvalencia@users.noreply.github.com>
2022-06-01 18:38:28 -0500

    Update test_persistencia.py

--- test_persistencia.py
+++ test_persistencia.py
@@ -1,13 +1,18 @@
-from persistencia import guardar_pedido
+"""
+Pruebas Persistencia
+"""
+import persistencia
 
-with open("pedidos.txt", "w", encoding="utf-8") as file:
- file.write("")
- file.close()
-
- pedidos = [{"nombre": "Pedro", "apellidos": "Gil deDiego"},
-            {"nombre": "Michael", "apellidos": "Jordan"},
-            {"nombre": "Mauricio", "apellidos": "Valencia"}]
-
-for n in pedidos:
-    guardar_pedido(n["nombre"],n["apellidos"])
+def test_guardar_pedido():
+  """
+  Prueba general
+  """
+  with open("pedidos.txt", "w+", encoding="utf-8") as file:
+    persistencia.guardar_pedido("Pedro", "Gil de Diego")
+    persistencia.guardar_pedido("Michael", "Jordan")
+    firstline = file.readline()
+    secondline = file.readline()
+    file.close()
+assert firstline == "-Pedro Gil de Diego\n"
+assert secondline == "-Michael Jordan\n"
 
