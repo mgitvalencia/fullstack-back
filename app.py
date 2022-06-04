791892d50ab62a2a7b2c46a6583d7e794f5d7a1d
mgitvalencia <105307230+mgitvalencia@users.noreply.github.com>
2022-06-04 18:29:58 -0500

    Validacion disponibilidad

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Pruebas Backend"""
 
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, Response
 from persistencia import guardar_pedido
 
 app = Flask(__name__)
@@ -14,3 +14,13 @@ def pizza():
     print(apellido)
     guardar_pedido(nombre,apellido)
     return redirect("http://localhost/naxer/solicita_pedido.html", code=302)
+    
+@app.route("/checksize",methods=['POST'])
+def checksize():
+    """Validar disponibilidad"""
+    tamano = request.form.get("tamano")
+    mensaje = "Disponible"
+    if tamano == "S":
+       mensaje = "No Disponible"
+    print(mensaje) 
+    return Response(mensaje, 200, {'Access-Control-Allow-Origin': '*'})
